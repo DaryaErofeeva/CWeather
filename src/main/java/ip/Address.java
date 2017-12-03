8fe06f7a40a53871f8ebec5914d387ef62d01b77
Unknown <da.erofeeva@gmail.com>
2017-12-03 12:29:13 +0200

    Add class Address  for getting IP

---
+++ src/main/java/ip/Address.java
@@ -0,0 +1,28 @@
+package ip;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Address {
+    private static String address;
+
+    private Address() {
+        try {
+            URL url = new URL("http://checkip.amazonaws.com/");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            address = br.readLine();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized static String getIPAddress() {
+        if (address == null) new Address();
+        return address;
+    }
+}
