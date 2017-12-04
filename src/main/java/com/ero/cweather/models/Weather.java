f8bc3c800e1b8f26c7306ec914acb788befc5499
Unknown <da.erofeeva@gmail.com>
2017-12-04 21:58:05 +0200

    Change task.java to Weather.java

---
+++ src/main/java/com/ero/cweather/models/Weather.java
@@ -0,0 +1,89 @@
+package com.ero.cweather.models;
+
+public class Weather {
+    private String title;
+    private Double temp;
+    private Double wind_speed;
+    private Boolean cloudy;
+    private Boolean raining;
+    private Boolean snowing;
+    private Boolean finished;
+    private Integer duration;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public Double getWindSpeed() {
+        return wind_speed;
+    }
+
+    public void setWindSpeed(double wind_speed) {
+        this.wind_speed = wind_speed;
+    }
+
+    public Boolean isCloudy() {
+        return cloudy;
+    }
+
+    public void setCloudy(boolean cloudy) {
+        this.cloudy = cloudy;
+    }
+
+    public Boolean isRaining() {
+        return raining;
+    }
+
+    public void setRaining(boolean raining) {
+        this.raining = raining;
+    }
+
+    public Boolean isSnowing() {
+        return snowing;
+    }
+
+    public void setSnowing(boolean snowing) {
+        this.snowing = snowing;
+    }
+
+    public Boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherSearcher{" +
+                "title='" + title + '\'' +
+                ", temp=" + temp +
+                ", wind_speed=" + wind_speed +
+                ", cloudy=" + cloudy +
+                ", raining=" + raining +
+                ", finished=" + finished +
+                ", duration=" + duration +
+                '}';
+    }
+}
