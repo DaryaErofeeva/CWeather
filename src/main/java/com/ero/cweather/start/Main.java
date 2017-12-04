d9f2e1afaf087e7575106e07255b9f3e3de8cb58
Unknown <da.erofeeva@gmail.com>
2017-12-04 22:06:07 +0200

    Add setting mainStage to Main.java

--- src/main/java/com/ero/cweather/start/Main.java
+++ src/main/java/com/ero/cweather/start/Main.java
@@ -1,5 +1,6 @@
 package com.ero.cweather.start;
 
+import com.ero.cweather.controllers.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,6 +13,8 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getClassLoader().getResource("fxml/Main.fxml"));
         Parent fxmlMain = fxmlLoader.load();
+        MainController mainController = fxmlLoader.getController();
+        mainController.setMainStage(stage);
 
         stage.setScene(new Scene(fxmlMain, 300, 300));
         stage.show();
