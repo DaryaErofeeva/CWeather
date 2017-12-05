f2f1f3620f2abe893ef44e63fce2cd61e3dde018
Unknown <da.erofeeva@gmail.com>
2017-12-05 16:26:07 +0200

    Fill WindController.java

--- src/main/java/com/ero/cweather/controllers/WindController.java
+++ src/main/java/com/ero/cweather/controllers/WindController.java
@@ -1,4 +1,45 @@
 package com.ero.cweather.controllers;
 
+import com.ero.cweather.models.Weather;
+import com.jfoenix.controls.JFXSlider;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class WindController {
+    @FXML
+    public JFXSlider sldrWind;
+
+    private Weather weather;
+    private boolean submitted;
+
+    public void onOKActionListener(ActionEvent actionEvent) {
+        weather.setWindSpeed(sldrWind.getValue());
+        submitted = true;
+        onCancelActionListener(actionEvent);
+    }
+
+    public Weather getWeather() {
+        return weather;
+    }
+
+    public void setWeather(Weather weather) {
+        this.weather = weather;
+
+        if (weather.getWindSpeed() != null)
+            sldrWind.setValue(weather.getWindSpeed());
+
+        submitted = false;
+    }
+
+    public boolean isSubmitted() {
+        return submitted;
+    }
+
+    public void onCancelActionListener(ActionEvent actionEvent) {
+        Node node = (Node) actionEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.hide();
+    }
 }
