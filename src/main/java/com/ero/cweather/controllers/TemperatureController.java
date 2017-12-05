6766a617c2f3be8121ec91f6fce074d390c0a72e
Unknown <da.erofeeva@gmail.com>
2017-12-05 16:26:31 +0200

    Fill TemperatureController.java

--- src/main/java/com/ero/cweather/controllers/TemperatureController.java
+++ src/main/java/com/ero/cweather/controllers/TemperatureController.java
@@ -1,4 +1,46 @@
 package com.ero.cweather.controllers;
 
+import com.ero.cweather.models.Weather;
+import com.jfoenix.controls.JFXSlider;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class TemperatureController {
+    @FXML
+    public JFXSlider sldrTemperature;
+
+    private Weather weather;
+    private boolean submitted;
+
+    public void onOKActionListener(ActionEvent actionEvent) {
+        weather.setTemp(sldrTemperature.getValue());
+        submitted = true;
+        onCancelActionListener(actionEvent);
+    }
+
+    public Weather getWeather() {
+        return weather;
+    }
+
+    public void setWeather(Weather weather) {
+        this.weather = weather;
+
+        if (weather.getTemp() != null)
+            sldrTemperature.setValue(weather.getTemp());
+
+        submitted = false;
+    }
+
+    public boolean isSubmitted() {
+        return submitted;
+    }
+
+    public void onCancelActionListener(ActionEvent actionEvent) {
+        Node node = (Node) actionEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.hide();
+    }
 }
