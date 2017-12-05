919859b9ad61e2a112364605830310258414efa7
Unknown <da.erofeeva@gmail.com>
2017-12-05 15:20:31 +0200

    Fill TitleController.java

--- src/main/java/com/ero/cweather/controllers/TitleController.java
+++ src/main/java/com/ero/cweather/controllers/TitleController.java
@@ -1,4 +1,44 @@
 package com.ero.cweather.controllers;
 
+import com.ero.cweather.models.Weather;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class TitleController {
+
+    @FXML
+    public JFXTextField txtTitle;
+
+    private Weather weather;
+    private boolean submitted;
+
+    public void onOKActionListener(ActionEvent actionEvent) {
+        weather.setTitle(txtTitle.getText());
+        submitted = true;
+        onCancelActionListener(actionEvent);
+    }
+
+    public Weather getWeather() {
+        return weather;
+    }
+
+    public void setWeather(Weather weather) {
+        this.weather = weather;
+        txtTitle.setText(weather.getTitle());
+
+        submitted = false;
+    }
+
+    public boolean isSubmitted() {
+        return submitted;
+    }
+
+    public void onCancelActionListener(ActionEvent actionEvent) {
+        Node node = (Node) actionEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.hide();
+    }
 }
