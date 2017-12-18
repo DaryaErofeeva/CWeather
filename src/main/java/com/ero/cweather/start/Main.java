048283e9152f2f1e78d3f3aadc918b8eee2a3ba6
Unknown <da.erofeeva@gmail.com>
2017-12-18 13:01:13 +0200

    Change stage size

--- src/main/java/com/ero/cweather/start/Main.java
+++ src/main/java/com/ero/cweather/start/Main.java
@@ -1,9 +1,6 @@
 package com.ero.cweather.start;
 
 import com.ero.cweather.controllers.MainController;
-import com.ero.cweather.db.WeatherDAO;
-import com.ero.cweather.db.collections.WeatherCollection;
-import com.ero.cweather.models.Weather;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,12 +13,16 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getClassLoader().getResource("fxml/Main.fxml"));
         Parent fxmlMain = fxmlLoader.load();
+
         MainController mainController = fxmlLoader.getController();
         mainController.setMainStage(stage);
 
-        stage.setScene(new Scene(fxmlMain));
+        stage.setMinHeight(340);
         stage.setMinWidth(320);
-        stage.setMinHeight(315);
+        stage.setTitle("CWeather");
+
+
+        stage.setScene(new Scene(fxmlMain));
         stage.show();
     }
 
