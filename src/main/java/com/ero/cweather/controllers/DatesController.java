4f35b69afb9b74a81f42765d147db84ce7087b30
Unknown <da.erofeeva@gmail.com>
2018-01-03 23:28:02 +0200

    Change view of Dates window

--- src/main/java/com/ero/cweather/controllers/DatesController.java
+++ src/main/java/com/ero/cweather/controllers/DatesController.java
@@ -1,10 +1,15 @@
 package com.ero.cweather.controllers;
 
+import com.ero.cweather.models.Weather;
+import com.ero.cweather.weather.WeatherSearcher;
+import com.ero.cweather.weather.weatherlibrary.datamodel.Forecastday;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -15,72 +20,56 @@ public class DatesController {
     @FXML
     FlowPane flowPane;
 
-    public void setDates(List<String> dates) {
+    private String color;
+    private Weather weather;
+
+    public void setDates(Weather weather, String color) {
+        this.color = color;
+        this.weather = weather;
+
         flowPane.getChildren().clear();
-        for (String date : dates)
-            flowPane.getChildren().add(createChild(date, dates.indexOf(date)));
+        List<Forecastday> forecastdays = WeatherSearcher.getDays(weather);
+        for (Forecastday forecastday : forecastdays)
+            flowPane.getChildren().add(createChild(forecastday));
     }
 
-    private Node createChild(String date, int index) {
+
+    private Node createChild(Forecastday forecastday) {
         VBox child = new VBox();
-        String headerColor = getDefaultColor(index);
-        child.setStyle("-fx-background-color: " + headerColor);
-        child.setMinWidth(250);
-        child.setAlignment(Pos.CENTER);
 
-        Label datesLabel = new Label(date);
-        datesLabel.setFont(Font.font("Ayuthaya", 30));
-        datesLabel.setTextFill(Color.WHITE);
+        // create content
+        VBox header = new VBox();
+        header.setStyle("-fx-background-color: " + color);
+        header.setAlignment(Pos.CENTER);
+        header.setPadding(new Insets(10, 10, 10, 10));
 
-        child.getChildren().add(datesLabel);
+        Label minTemp = new Label("Минимальная температура " + forecastday.getDay().mintemp_c);
+        Label maxTemp = new Label("Максимальная температура " + forecastday.getDay().maxtemp_c);
+        Label wind = new Label("Максимальная скорость ветра " + forecastday.getDay().maxwind_mph + " м/час");
+
+        minTemp.setFont(Font.font("Ayuthaya", 14));
+        minTemp.setTextFill(Color.WHITE);
+
+        maxTemp.setFont(Font.font("Ayuthaya", 14));
+        maxTemp.setTextFill(Color.WHITE);
+
+        wind.setFont(Font.font("Ayuthaya", 14));
+        wind.setTextFill(Color.WHITE);
+
+        header.getChildren().addAll(minTemp, maxTemp, wind);
+
+
+        StackPane footer = new StackPane();
+        footer.setStyle("-fx-background-color: white");
+
+        Label titleLabel = new Label(forecastday.date.substring(8) + "-" + forecastday.date.substring(5, 7));
+        titleLabel.setTextFill(Color.web(color));
+        titleLabel.setFont(Font.font("Ayuthaya", 20));
+        footer.getChildren().add(titleLabel);
+        footer.setPadding(new Insets(10, 10, 10, 10));
+
+        child.getChildren().addAll(header, footer);
+
         return child;
-    }
-
-    private String getDefaultColor(int i) {
-        String color = "#FFFFFF";
-        switch (i) {
-            case 0:
-                color = "#8F3F7E";
-                break;
-            case 1:
-                color = "#B5305F";
-                break;
-            case 2:
-                color = "#CE584A";
-                break;
-            case 3:
-                color = "#DB8D5C";
-                break;
-            case 4:
-                color = "#DA854E";
-                break;
-            case 5:
-                color = "#E9AB44";
-                break;
-            case 6:
-                color = "#FEE435";
-                break;
-            case 7:
-                color = "#99C286";
-                break;
-            case 8:
-                color = "#01A05E";
-                break;
-            case 9:
-                color = "#4A8895";
-                break;
-            case 10:
-                color = "#16669B";
-                break;
-            case 11:
-                color = "#2F65A5";
-                break;
-            case 12:
-                color = "#4E6A9C";
-                break;
-            default:
-                break;
-        }
-        return color;
     }
 }
